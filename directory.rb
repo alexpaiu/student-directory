1959589e48fc9b5780e54400b388ae316443b317
alexpaiu <alex.paiu23@gmail.com>
2019-08-02 12:37:15 +0100

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,21 +1,31 @@
 students = [
-	"Dr. Hanibbal Lecter",
-	"Darth Vader",
-	"Nurse Ratched",
-	"Michael Corleone",
-	"Alex DeLarge",
-	"The Wicked Witch of the West",
-	"Terminator",
-	"Freddy Krueger",
-	"The Joker",
-	"Joffrey Baratheob",
-	"Norman Bates"
+	{name: "Dr. Hanibbal Lecter", cohort: :november} ,
+	{name: "Darth Vader", cohort:  :november} ,
+	{name: "Nurse Ratched", cohort:  :november} ,
+	{name: "Michael Corleone", cohort:  :november} ,
+	{name: "Alex DeLarge", cohort:  :november} ,
+	{name: "The Wicked Witch of the West", cohort:  :november} ,
+	{name: "Terminator", cohort:  :november} ,
+	{name: "Freddy Krueger", cohort:  :november} ,
+	{name: "The Joker", cohort:  :november} ,
+	{name: "Joffrey Baratheob", cohort:  :november} ,
+	{name: "Norman Bates", cohort:  :november}
 ]
-puts "The studens of Villains Academy"
-puts "---------------"
-students.each do |student|
-	puts student
+def print_header
+  puts "The studens of Villains Academy"
+  puts "---------------"
 end
 
-puts "Overall, we have #{students.count} great students. "
+def print(students)
+  students.each do |students|
+	puts "#{students[:name]} (#{students[:cohort]} cohort)"
+  end
+end
 
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students. "
+end
+
+print_header
+print(students)
+print_footer(students)
