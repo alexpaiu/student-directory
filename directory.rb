ce5269319b30522fbe58795dc30e502a5e48385f
alexpaiu <alex.paiu23@gmail.com>
2019-08-02 11:48:31 +0100

    third commit

---
+++ directory.rb
@@ -0,0 +1,20 @@
+#first we print the list of students
+puts "The studens of Villains Academy"
+puts "---------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+#we will print the total number of students
+print "Overall, we have "
+#it's important that print() doesn't add a new line character
+print 11 
+print " great students."
+
